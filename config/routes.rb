b4f8da563bbfdd280d07698da22ffa9f3e5921ce
jfoody6 <164776412+jfoody6@users.noreply.github.com>
2024-05-11 17:58:33 +0000

    added user accounts sign in nav

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
+  # config/routes.rb
+
+  root to: "boards#index"
+
   get("/", { :controller => "boards", :action => "index" })
 
   # Routes for the Post resource:
